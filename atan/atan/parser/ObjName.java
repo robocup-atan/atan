5853312e175839a93f2626b91d152d1fc37d3d21
(no author) <(no author)@accd74b2-c9f6-420e-bc6b-c3ab7889e624>
2003-09-13 17:09:55 +0000

    This commit was manufactured by cvs2svn to create tag 'arelease'.

--- atan/atan/parser/ObjName.java
+++
@@ -1,8 +0,0 @@
-package atan.parser;
-
-import atan.model.*;
-
-public interface ObjName {
-    public void infoSeeFromEast(Controller c, double dist, double dir);
-    public void infoSeeFromWest(Controller c, double dist, double dir);
-}
