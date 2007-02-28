3a150f5b8c30c7126113fdcdd23b2fe5fc8f323a
wwan <wwan@accd74b2-c9f6-420e-bc6b-c3ab7889e624>
2007-02-28 19:04:59 +0000

    order of flags for west player was changed

--- atan/java/atan/parser/ObjNameFlagRight.java
+++ atan/java/atan/parser/ObjNameFlagRight.java
@@ -23,18 +23,18 @@ class ObjNameFlagRight implements ObjName {
       }
     }
     public void infoSeeFromWest(Controller c, double dist, double dir) {
-      switch (qualifier) {
-        case 't': switch (number) {
-          case 30: c.infoSeeFlagOther(Flag.FLAG_RIGHT_30, dist, dir); break;
-          case 20: c.infoSeeFlagOther(Flag.FLAG_RIGHT_20, dist, dir); break;
-          case 10: c.infoSeeFlagOther(Flag.FLAG_RIGHT_10, dist, dir); break;
-        }; break;
-        case 'b': switch (number) {
-          case 30: c.infoSeeFlagOther(Flag.FLAG_LEFT_30, dist, dir); break;
-          case 20: c.infoSeeFlagOther(Flag.FLAG_LEFT_20, dist, dir); break;
-          case 10: c.infoSeeFlagOther(Flag.FLAG_LEFT_10, dist, dir); break;
-        }; break;
-        case '0': c.infoSeeFlagOther(Flag.FLAG_CENTER, dist, dir); break;
+        switch (qualifier) {
+          case 't': switch (number) {
+            case 30: c.infoSeeFlagOther(Flag.FLAG_LEFT_30, dist, dir); break;
+            case 20: c.infoSeeFlagOther(Flag.FLAG_LEFT_20, dist, dir); break;
+            case 10: c.infoSeeFlagOther(Flag.FLAG_LEFT_10, dist, dir); break;
+          }; break;
+          case 'b': switch (number) {
+            case 30: c.infoSeeFlagOther(Flag.FLAG_RIGHT_30, dist, dir); break;
+            case 20: c.infoSeeFlagOther(Flag.FLAG_RIGHT_20, dist, dir); break;
+            case 10: c.infoSeeFlagOther(Flag.FLAG_RIGHT_10, dist, dir); break;
+          }; break;
+          case '0': c.infoSeeFlagOther(Flag.FLAG_CENTER, dist, dir); break;
+        }
       }
-    }
 }
