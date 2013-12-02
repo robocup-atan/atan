75223bf7b8890700be50814cd18046ca25a225ec
Nick James <verbitan@gmail.com>
2013-12-02 20:44:13 +0000

    Add player hear referee tests.

--- src/test/java/com/github/robocup_atan/atan/parser/player/PlayerHearCommandTest.java
+++ src/test/java/com/github/robocup_atan/atan/parser/player/PlayerHearCommandTest.java
@@ -1,73 +1,148 @@
 package com.github.robocup_atan.atan.parser.player;
 
+import com.github.robocup_atan.atan.model.enums.PlayMode;
 import com.github.robocup_atan.atan.parser.BaseCommandFilter;
 import com.github.robocup_atan.atan.parser.Filter;
 import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized;
 
 /**
- * Test class for parsing the player init command.
+ * Test class for parsing the player hear command.
  *
  * @author Atan
  */
+@RunWith(Parameterized.class)
 public class PlayerHearCommandTest {
 
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                // East team referee messages.
+                {"(hear 1 referee before_kick_off)",   true,  PlayMode.BEFORE_KICK_OFF},
+                {"(hear 1 referee time_over)",         true,  PlayMode.TIME_OVER},
+                {"(hear 1 referee play_on)",           true,  PlayMode.PLAY_ON},
+                {"(hear 1 referee kick_off_l)",        true,  PlayMode.KICK_OFF_OTHER},
+                {"(hear 1 referee kick_off_r)",        true,  PlayMode.KICK_OFF_OWN},
+                {"(hear 1 referee kick_in_l)",         true,  PlayMode.KICK_IN_OTHER},
+                {"(hear 1 referee kick_in_r)",         true,  PlayMode.KICK_IN_OWN},
+                {"(hear 1 referee free_kick_l)",       true,  PlayMode.FREE_KICK_OTHER},
+                {"(hear 1 referee free_kick_r)",       true,  PlayMode.FREE_KICK_OWN},
+                {"(hear 1 referee free_kick_fault_l)", true,  PlayMode.FREE_KICK_FAULT_OTHER},
+                {"(hear 1 referee free_kick_fault_r)", true,  PlayMode.FREE_KICK_FAULT_OWN},
+                {"(hear 1 referee corner_kick_l)",     true,  PlayMode.CORNER_KICK_OTHER},
+                {"(hear 1 referee corner_kick_r)",     true,  PlayMode.CORNER_KICK_OWN},
+                {"(hear 1 referee goal_kick_l)",       true,  PlayMode.GOAL_KICK_OTHER},
+                {"(hear 1 referee goal_kick_r)",       true,  PlayMode.GOAL_KICK_OWN},
+                {"(hear 1 referee goal_l_1)",          true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_l_2)",          true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_l_3)",          true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_l_4)",          true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_l_5)",          true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_l_6)",          true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_l_7)",          true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_l_8)",          true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_l_9)",          true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_l_10)",         true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_l_11)",         true,  PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_1)",          true,  PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_2)",          true,  PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_3)",          true,  PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_4)",          true,  PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_5)",          true,  PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_6)",          true,  PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_7)",          true,  PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_8)",          true,  PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_9)",          true,  PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_10)",         true,  PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_11)",         true,  PlayMode.GOAL_OWN},
+                // West team referee messages.
+                {"(hear 1 referee before_kick_off)",   false, PlayMode.BEFORE_KICK_OFF},
+                {"(hear 1 referee time_over)",         false, PlayMode.TIME_OVER},
+                {"(hear 1 referee play_on)",           false, PlayMode.PLAY_ON},
+                {"(hear 1 referee kick_off_l)",        false, PlayMode.KICK_OFF_OWN},
+                {"(hear 1 referee kick_off_r)",        false, PlayMode.KICK_OFF_OTHER},
+                {"(hear 1 referee kick_in_l)",         false, PlayMode.KICK_IN_OWN},
+                {"(hear 1 referee kick_in_r)",         false, PlayMode.KICK_IN_OTHER},
+                {"(hear 1 referee free_kick_l)",       false, PlayMode.FREE_KICK_OWN},
+                {"(hear 1 referee free_kick_r)",       false, PlayMode.FREE_KICK_OTHER},
+                {"(hear 1 referee free_kick_fault_l)", false, PlayMode.FREE_KICK_FAULT_OWN},
+                {"(hear 1 referee free_kick_fault_r)", false, PlayMode.FREE_KICK_FAULT_OTHER},
+                {"(hear 1 referee corner_kick_l)",     false, PlayMode.CORNER_KICK_OWN},
+                {"(hear 1 referee corner_kick_r)",     false, PlayMode.CORNER_KICK_OTHER},
+                {"(hear 1 referee goal_kick_l)",       false, PlayMode.GOAL_KICK_OWN},
+                {"(hear 1 referee goal_kick_r)",       false, PlayMode.GOAL_KICK_OTHER},
+                {"(hear 1 referee goal_l_1)",          false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_l_2)",          false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_l_3)",          false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_l_4)",          false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_l_5)",          false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_l_6)",          false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_l_7)",          false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_l_8)",          false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_l_9)",          false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_l_10)",         false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_l_11)",         false, PlayMode.GOAL_OWN},
+                {"(hear 1 referee goal_r_1)",          false, PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_2)",          false, PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_3)",          false, PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_4)",          false, PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_5)",          false, PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_6)",          false, PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_7)",          false, PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_8)",          false, PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_9)",          false, PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_10)",         false, PlayMode.GOAL_OTHER},
+                {"(hear 1 referee goal_r_11)",         false, PlayMode.GOAL_OTHER},
+
+            });
+    }
+
     // Global test variables.
-    CmdParserPlayer parser;
-    TestFilter commandFilter;
-    Filter filter;
-    
-    // Current test expected values.
-    String currentCommand;
-    double expectedDirection;
-    String expectedMessage;
+    private CmdParserPlayer parser;
+    private TestFilter      commandFilter;
+    private Filter          filter;
+
+    // Test parameters.
+    private String   command;
+    private boolean  isTeamEast;
+    private PlayMode expectedPlayMode;
+
+    public PlayerHearCommandTest(
+        String   command,
+        boolean  isTeamEast,
+        PlayMode playMode) {
+
+        this.command          = command;
+        this.isTeamEast       = isTeamEast;
+        this.expectedPlayMode = playMode;
+    }
 
     @Before
     public void setUpTest() throws Exception {
-        parser = new CmdParserPlayer(new StringReader(""));
+        parser        = new CmdParserPlayer(new StringReader(""));
         commandFilter = new TestFilter();
-        filter = new Filter();
+        filter        = new Filter();
     }
 
-    // Tests
     @Test
-    public void hearOther180() {
-        setCommand("(hear 1 180 RunFaster)");
-        setExpected(180, "RunFaster");
-        runTest();
-    }
-    
-    @Test
-    public void hearOtherNeg180() {
-        setCommand("(hear 1 -180 RunFaster)");
-        setExpected(-180, "RunFaster");
-        runTest();
+    public void parseCommand() {
+        filter.run(command, commandFilter);
     }
 
-    // Private methods to make writing tests simpler.
-    private void setCommand(String command) {
-        currentCommand = command;
-    }
-
-    private void setExpected(double direction, String message) {
-        expectedDirection = direction;
-        expectedMessage = message;
-    }
-
-    private void runTest() {
-        filter.run(currentCommand, commandFilter);
-    }
-
-    // Private classes for these tests.
     private class TestFilter extends BaseCommandFilter {
 
         private TestController controller = new TestController();
-        private BaseActionsPlayer action = new BaseActionsPlayer();
+        private TestActions    action     = new TestActions();
 
         @Override
         public void hearCommand(String cmd) {
@@ -82,9 +157,16 @@ public class PlayerHearCommandTest {
     private class TestController extends BaseControllerPlayer {
 
         @Override
-        public void infoHearPlayer(double direction, String message) {
-            Assert.assertEquals(expectedDirection, direction, 0);
-            Assert.assertEquals(expectedMessage, message);
+        public void infoHearPlayMode(PlayMode playMode) {
+            Assert.assertEquals(expectedPlayMode, playMode);
+        }
+    }
+
+    private class TestActions extends BaseActionsPlayer {
+
+        @Override
+        public boolean isTeamEast() {
+            return isTeamEast;
         }
     }
     
@@ -104,7 +186,8 @@ public class PlayerHearCommandTest {
         
         @Override
         protected void finished(Description description) {
-            System.out.println(description.getMethodName());
+            System.out.println(command);
         }
     };
 }
+
