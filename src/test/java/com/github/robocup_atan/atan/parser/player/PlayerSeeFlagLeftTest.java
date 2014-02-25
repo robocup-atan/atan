8000fec48aaf8732c9d9ddb62aaf5f966a75f796
Nick James <verbitan@gmail.com>
2014-02-25 20:41:32 +0000

    Add player see flag left/right tests

---
+++ src/test/java/com/github/robocup_atan/atan/parser/player/PlayerSeeFlagLeftTest.java
@@ -0,0 +1,188 @@
+package com.github.robocup_atan.atan.parser.player;
+
+import com.github.robocup_atan.atan.model.enums.Flag;
+import com.github.robocup_atan.atan.parser.BaseCommandFilter;
+import com.github.robocup_atan.atan.parser.Filter;
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized;
+
+/**
+ * Test class for parsing the player see command.
+ *
+ * @author Atan
+ */
+@RunWith(Parameterized.class)
+public class PlayerSeeFlagLeftTest {
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+
+                /**
+                 * Tests for seeing the left flags are captured in the following
+                 * array structure.
+                 *   0: Server command.
+                 *   1: Value of isTeamEast.
+                 *   2: Expected value of flag.
+                 *   3: Expected value of distance.
+                 *   4: Expected value of direction.
+                 *   5: Expected value of distance change.
+                 *   6: Expected value of direction change.
+                 *   7: Expected value of body facing direction.
+                 *   8: Expected value of head facing direction.
+                 */
+
+                // East team see left flag messages.
+                {"(see 123.45 ((f b r 50) 123.45 123.45 123.45 123.45 123.45 123.45))", true,  Flag.OWN_50,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f b r 40) 123.45 123.45 123.45 123.45 123.45 123.45))", true,  Flag.OWN_40,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f b r 30) 123.45 123.45 123.45 123.45 123.45 123.45))", true,  Flag.OWN_30,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f b r 20) 123.45 123.45 123.45 123.45 123.45 123.45))", true,  Flag.OWN_20,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f b r 10) 123.45 123.45 123.45 123.45 123.45 123.45))", true,  Flag.OWN_10,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f b 0) 123.45 123.45 123.45 123.45 123.45 123.45))",    true,  Flag.CENTER,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f b l 10) 123.45 123.45 123.45 123.45 123.45 123.45))", true,  Flag.OTHER_10, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f b l 20) 123.45 123.45 123.45 123.45 123.45 123.45))", true,  Flag.OTHER_20, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f b l 30) 123.45 123.45 123.45 123.45 123.45 123.45))", true,  Flag.OTHER_30, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f b l 40) 123.45 123.45 123.45 123.45 123.45 123.45))", true,  Flag.OTHER_40, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f b l 50) 123.45 123.45 123.45 123.45 123.45 123.45))", true,  Flag.OTHER_50, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                // West team see left flag messages.
+                {"(see 123.45 ((f t l 50) 123.45 123.45 123.45 123.45 123.45 123.45))", false, Flag.OWN_50,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f t l 40) 123.45 123.45 123.45 123.45 123.45 123.45))", false, Flag.OWN_40,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f t l 30) 123.45 123.45 123.45 123.45 123.45 123.45))", false, Flag.OWN_30,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f t l 20) 123.45 123.45 123.45 123.45 123.45 123.45))", false, Flag.OWN_20,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f t l 10) 123.45 123.45 123.45 123.45 123.45 123.45))", false, Flag.OWN_10,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f t 0) 123.45 123.45 123.45 123.45 123.45 123.45))",    false, Flag.CENTER,   123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f t r 10) 123.45 123.45 123.45 123.45 123.45 123.45))", false, Flag.OTHER_10, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f t r 20) 123.45 123.45 123.45 123.45 123.45 123.45))", false, Flag.OTHER_20, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f t r 30) 123.45 123.45 123.45 123.45 123.45 123.45))", false, Flag.OTHER_30, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f t r 40) 123.45 123.45 123.45 123.45 123.45 123.45))", false, Flag.OTHER_40, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+                {"(see 123.45 ((f t r 50) 123.45 123.45 123.45 123.45 123.45 123.45))", false, Flag.OTHER_50, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45},
+
+            });
+    }
+
+    // Global test variables.
+    private CmdParserPlayer parser;
+    private TestFilter      commandFilter;
+    private Filter          filter;
+
+    // Test parameters.
+    private String  command;
+    private boolean isTeamEast;
+    private Flag    expectedFlag;
+    private double  expectedDistance;
+    private double  expectedDirection;
+    private double  expectedDistChange;
+    private double  expectedDirChange;
+    private double  expectedBodyFacingDirection;
+    private double  expectedHeadFacingDirection;
+
+    public PlayerSeeFlagLeftTest(
+        String  command,
+        boolean isTeamEast,
+        Flag    flag,
+        double  distance,
+        double  direction,
+        double  distChange,
+        double  dirChange,
+        double  bodyFacingDirection,
+        double  headFacingDirection) {
+
+        this.command                     = command;
+        this.isTeamEast                  = isTeamEast;
+        this.expectedFlag                = flag;
+        this.expectedDistance            = distance;
+        this.expectedDirection           = direction;;
+        this.expectedDistChange          = distChange;
+        this.expectedDirChange           = dirChange;
+        this.expectedBodyFacingDirection = bodyFacingDirection;
+        this.expectedHeadFacingDirection = headFacingDirection;
+    }
+
+    @Before
+    public void setUpTest() throws Exception {
+        parser        = new CmdParserPlayer(new StringReader(""));
+        commandFilter = new TestFilter();
+        filter        = new Filter();
+    }
+
+    @Test
+    public void parseCommand() {
+        filter.run(command, commandFilter);
+    }
+
+    private class TestFilter extends BaseCommandFilter {
+
+        private TestController controller = new TestController();
+        private TestActions    action     = new TestActions();
+
+        @Override
+        public void seeCommand(String cmd) {
+            try {
+                parser.parseSeeCommand(cmd, controller, action);
+            } catch (ParseException ex) {
+                Assert.fail(ex.getMessage());
+            }
+        }
+    }
+
+    private class TestController extends BaseControllerPlayer {
+
+        @Override
+        public void infoSeeFlagLeft(
+            Flag flag,
+            double distance,
+            double direction,
+            double distChange,
+            double dirChange,
+            double bodyFacingDirection,
+            double headFacingDirection) {
+
+            Assert.assertEquals(expectedFlag, flag);
+            Assert.assertEquals(expectedDistance, distance, 0);
+            Assert.assertEquals(expectedDirection, direction, 0);
+            Assert.assertEquals(expectedDistChange, distChange, 0);
+            Assert.assertEquals(expectedDirChange, dirChange, 0);
+            Assert.assertEquals(expectedBodyFacingDirection, bodyFacingDirection, 0);
+            Assert.assertEquals(expectedHeadFacingDirection, headFacingDirection, 0);
+        }
+    }
+
+    private class TestActions extends BaseActionsPlayer {
+
+        @Override
+        public boolean isTeamEast() {
+            return isTeamEast;
+        }
+    }
+
+    // Test logging
+    @Rule
+    public TestWatcher watchman = new TestWatcher() {
+
+        @Override
+        protected void succeeded(Description description) {
+            System.out.print("Passed - ");
+        }
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            System.out.print("FAILED - ");
+        }
+
+        @Override
+        protected void finished(Description description) {
+            System.out.println(command);
+        }
+    };
+}
+
