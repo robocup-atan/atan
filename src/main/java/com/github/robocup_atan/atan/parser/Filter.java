f3bd83eb874d7b1b86ae3ee0d37c7f74b3daabb8
Nick James <verbitan@gmail.com>
2014-11-29 17:12:30 +0000

    Add tests for invalid commands.

--- src/main/java/com/github/robocup_atan/atan/parser/Filter.java
+++ src/main/java/com/github/robocup_atan/atan/parser/Filter.java
@@ -26,6 +26,8 @@ package com.github.robocup_atan.atan.parser;
  * #L%
  */
 
+import java.lang.IllegalArgumentException;
+
 /**
  * This class takes the input string from the SServer and determines which
  * type of command it is. (see, hear, sense body, ...)
@@ -34,6 +36,22 @@ package com.github.robocup_atan.atan.parser;
  */
 public class Filter {
 
+    private enum CommandWords {
+        change_player_type,
+        error,
+        hear,
+        init,
+        ok,
+        player_param,
+        player_type,
+        see,
+        see_global,
+        sense_body,
+        server_param,
+        warning,
+        INVALID_COMMAND_WORD
+    }
+
     /**
      * Works out what type of command has been put into the method.
      *
@@ -41,49 +59,60 @@ public class Filter {
      * @param f An instance of CommandFilter.
      */
     public void run(String cmd, CommandFilter f) {
-        char c1, c2;
-        c1 = cmd.charAt(1);
-        switch (c1) {
-            case 's' :    // Server command begins with s (see, see_global, sense_body, server_param)
-                if ((c2 = cmd.charAt(3)) == 'e') {
-                    if ((cmd.charAt(5)) == 'g') {
-                        f.seeCommand(cmd.substring(12, cmd.length() - 1));
-                    } else {
-                        f.seeCommand(cmd.substring(5, cmd.length() - 1));
-                    }
-                } else if (c2 == 'n') {
-                    f.senseBodyCommand(cmd.substring(12, cmd.length() - 1));
-                } else if (c2 == 'r') {
-                    f.serverParamCommand(cmd.substring(14, cmd.length() - 1));
-                }
-                break;
-            case 'p' :    // Server command begins with p (player_param, player_type)
-                if ((c2 = cmd.charAt(8)) == 'p') {
-                    f.playerParamCommand(cmd.substring(14, cmd.length() - 1));
-                } else if (c2 == 't') {
-                    f.playerTypeCommand(cmd.substring(13, cmd.length() - 1));
-                }
-                break;
-            case 'i' :    // Server command begins with i (init)
-                f.initCommand(cmd.substring(6, cmd.length() - 1));
-                break;
-            case 'h' :    // Server command begins with h (hear)
-                f.hearCommand(cmd.substring(6, cmd.length() - 1));
-                break;
-            case 'o' :    // Server command begins with o (ok)
-                f.okCommand(cmd.substring(4, cmd.length() - 1));
-                break;
-            case 'w' :    // Server command begins with w (warning)
-                f.warningCommand(cmd.substring(9, cmd.length() - 1));
-                break;
-            case 'c' :    // Server command begins with c (change_player_type)
+
+        CommandWords commandWord = null;
+
+        if ( cmd.contains(" ") ) {
+            try {
+                commandWord = CommandWords.valueOf(cmd.substring(1, cmd.indexOf(" ")));
+            } catch (IllegalArgumentException e) {
+                commandWord = CommandWords.INVALID_COMMAND_WORD;
+            }
+        } else {
+            commandWord = CommandWords.INVALID_COMMAND_WORD;
+        }
+
+        switch (commandWord) {
+            case change_player_type:
                 f.changePlayerTypeCommand(cmd.substring(20, cmd.length() - 1));
                 break;
-            case 'e' :    // Server command begins with e (error)
+            case error:
                 f.errorCommand(cmd.substring(7, cmd.length() - 1));
                 break;
-            default :     // Unknown command
-                throw new Error("Invalid command'" + cmd + "'");
+            case hear:
+                f.hearCommand(cmd.substring(6, cmd.length() - 1));
+                break;
+            case init:
+                f.initCommand(cmd.substring(6, cmd.length() - 1));
+                break;
+            case ok:
+                f.okCommand(cmd.substring(4, cmd.length() - 1));
+                break;
+            case player_param:
+                f.playerParamCommand(cmd.substring(14, cmd.length() - 1));
+                break;
+            case player_type:
+                f.playerTypeCommand(cmd.substring(13, cmd.length() - 1));
+                break;
+            case see:
+                f.seeCommand(cmd.substring(5, cmd.length() - 1));
+                break;
+            case see_global:
+                f.seeCommand(cmd.substring(12, cmd.length() - 1));
+                break;
+            case sense_body:
+                f.senseBodyCommand(cmd.substring(12, cmd.length() - 1));
+                break;
+            case server_param:
+                f.serverParamCommand(cmd.substring(14, cmd.length() - 1));
+                break;
+            case warning :
+                f.warningCommand(cmd.substring(9, cmd.length() - 1));
+                break;
+            case INVALID_COMMAND_WORD :
+            default :
+                throw new Error("Invalid command received: \"" + cmd + "\"");
         }
     }
 }
+
